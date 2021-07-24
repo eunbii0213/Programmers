c4355054d9f29c4b64f3c9c768d685630f058a1e
은비 <67318165+eunbii0213@users.noreply.github.com>
2021-07-25 02:07:28 +0900

    Update 가장큰수.java

--- 가장큰수.java
+++ 가장큰수.java
@@ -1,114 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Comparator;
 
-public class Main{
+class Solution {
 
-    public static void main(String[] args) throws IOException {
-        
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(),"[,] ");
-        PriorityQueue<Integer> tenUnderQueue = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> tenOverQueue = new PriorityQueue<>(Collections.reverseOrder());
-        
-   
-        int tenUnder=0;
-        int tenOver=0;
-        String str1=null; //만든 수를 담음 
-        String str2;
-        
-        
-        
-        while(st.hasMoreTokens()) {
-        	
-        		str2 = st.nextToken();
-        		
-        		if(Integer.parseInt(str2)>=10) {
-        			
-        			tenOver++;
-        			tenOverQueue.add(Integer.parseInt(str2));
-        		}
-        		
-        		else {
-        			tenUnder++;
-        			tenUnderQueue.add(Integer.parseInt(str2));
-        			
-        		}
-        		
-        		
-        		
-        		
-        	}//while문 끝 숫자만 따로 분리 작업.
-        
-        
-        
-        Integer[] tenUnderArr = new Integer[tenUnder];
-		
-		 for(int i=0;i<tenUnder;i++) { 
-			 
-			 tenUnderArr[i] = tenUnderQueue.poll();
-			 
+	public String solution(int[] numbers) {
+
+		// 숫자를 문자열로 변환
+		String[] result = new String[numbers.length];
+		for (int i = 0; i < numbers.length; i++) {
+			result[i] = String.valueOf(numbers[i]);
 		}
-		 
-        
-        
-        Integer[] tenOverArr  = new Integer[tenOver];
-        
-        for(int i=0;i<tenOver;i++) { 
-			 
-			 tenOverArr[i] = tenOverQueue.poll();
-			 
-		}
-        
-       
-        
-		//int max=0;
-        
-        String number="";
-        int j=0;
-        for(int i=0;i<tenUnder;i++) {
-        	
-        	if(tenOverArr[j]/10 == tenUnderArr[i]) {
-        		
-        		if(j<tenOver) {
-        			
-	        		if((tenOverArr[j]%10)>tenUnderArr[i]) {
-	        			
-	        			number+=tenOverArr[j];
-	        			j++;
-	        			i--;
-	        			continue;
-	        		}
-	        		
-	        		else {
-	            		number+=tenUnderArr[i];
-	            		continue;
-	            	}
-        		}
-        		
-        	}//십의 자리 수가 같고 일의 자리수가 0이상이라면 십의 자리수를 넣는게 더 큰숫자임
-        	
-        	else {
-        		number+=tenUnderArr[i];
-        	}
-        	
-        	
-        }
-        
-       while(j<tenOver) {
-    	   
-    	   number+=tenOverArr[j];
-    	   j++;
-    	   
-       }
-        
 
-        System.out.println(number);
-        
+		// 정렬
+		Arrays.sort(result, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+
+				return ((o2 + o1).compareTo(o1 + o2));
+			}
+		});
+
+		// 0만 여러개 있는 배열의 경우 하나의 0만 리턴
+		if(result[0].equals("0")) {
+			return "0";
+		}
 		
-       
-    }
+		String answer = "";
+		// 정렬된 문자 하나로 합치기
+		for (String a : result) {
+			answer += a;
+		}
+		return answer;
+	}
 }
